4483d329b852ce1b7174737480ab172909708f31
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-01-05 20:17:46 +0900

    all css

--- src/components/check_mbti/check_mbti.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const CheckMbti = (props) => <h1>Choose your MBTI</h1>;
-
-export default CheckMbti;
---
+++ src/components/share/share.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Share = (props) => {
+  return (
+    <section>
+      <h1>share link!</h1>
+      <h3>공유할수록 수치는 정확해집니다</h3>
+      <ul>
+        <li>Kakao</li>
+        <li>link</li>
+        <li>instagram</li>
+        <li>twitter</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Share;
